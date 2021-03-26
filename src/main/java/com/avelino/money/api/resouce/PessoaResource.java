62c2d042a49985dbd3e48e82934ec605d3a42040
Carlos Avelino <carlos.avelino2.0@gmail.com>
2021-03-25 21:58:45 -0300

    Atualizando pessoa com PUT
    Criação da classe PessoaService

--- src/main/java/com/avelino/money/api/resouce/PessoaResource.java
+++ src/main/java/com/avelino/money/api/resouce/PessoaResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.avelino.money.api.event.RecursoCriadoEvent;
 import com.avelino.money.api.model.Pessoa;
 import com.avelino.money.api.repository.PessoaRepository;
+import com.avelino.money.api.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -26,6 +28,9 @@ public class PessoaResource {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private PessoaService pessoaService;
 	
 	@Autowired //dispara evento 
 	private ApplicationEventPublisher publisher;//publicador do evento de aplicação 
@@ -35,7 +40,7 @@ public class PessoaResource {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
-	}
+	} 
 
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
@@ -47,5 +52,11 @@ public class PessoaResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long codigo) {
 		pessoaRepository.delete(codigo);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa){
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
 	}
 }
