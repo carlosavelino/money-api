afe60c8e12064fe5b7eda1f57c1b4ea4c85fab01
Carlos Avelino <carlos.avelino2.0@gmail.com>
2021-04-08 22:05:35 -0300

    Criação da migração e entidade de lançamento

---
+++ src/main/java/com/avelino/money/api/model/TipoLancamento.java
@@ -0,0 +1,6 @@
+package com.avelino.money.api.model;
+
+public enum TipoLancamento {
+	RECEITA,
+	DESPESA
+}
--- src/main/java/com/avelino/money/api/resouce/PessoaResource.java
+++ src/main/java/com/avelino/money/api/resouce/PessoaResource.java
@@ -62,7 +62,7 @@ public class PessoaResource {
 	
 	@PutMapping("/codigo/ativo")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody(required = true) Boolean ativo) {
+	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
 		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
 	}
 }
